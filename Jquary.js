c75ed348a11833535b17d6e9178693f532718237
Ahmad <110009507+AhmadIDU@users.noreply.github.com>
2023-01-22 23:00:56 +0500

    bot qoshdim

--- Jquary.js
+++ Jquary.js
@@ -1,7 +1,8 @@
-$(document).ready(function(){ 
+$(document).ready(function(){
     let token = "5927919425:AAEXV9LjV1_qelM34Suev0uPcfWShBdwekI" 
     let userId = 1279418150
 
+    surName = $('#surName');
     nameInput = $('#name');
     phone = $('#phone');
     email = $('#email');
@@ -12,10 +13,10 @@ $(document).ready(function(){
         $.ajax({
             url:'https://api.telegram.org/bot'+token+'/sendMessage',
             method:'POST',
-            data: { chat_id : userId, text: "\n Name: " + nameInput.val() + 
+            data: { chat_id : userId, text: "\n Name: " + nameInput.val() + "\n SurNmae: "+ surName.val()+
              "\n Phone: " + phone.val() + "\n Email: " + email.val()+  "\n Message: " + message.val()},
 
-            success:function(succ){
+            success:function(success){
             
                 $('[type="submit"]').slideDown();
                 $('form').trigger("reset");
